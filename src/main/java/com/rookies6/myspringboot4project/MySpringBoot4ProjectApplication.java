f82c29037e3def0dd46e9650650f29bdc4ca8a16
surimchai <slchai0903@gmail.com>
2026-08-31 15:35:35 +0900

    MyRunner Update

--- src/main/java/com/rookies6/myspringboot4project/MySpringBoot4ProjectApplication.java
+++ src/main/java/com/rookies6/myspringboot4project/MySpringBoot4ProjectApplication.java
@@ -1,6 +1,7 @@
 package com.rookies6.myspringboot4project;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
@@ -8,7 +9,11 @@ import org.springframework.context.annotation.Bean;
 public class MySpringBoot4ProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MySpringBoot4ProjectApplication.class, args);
+        //SpringApplication.run(MySpringBoot4ProjectApplication.class, args);
+        SpringApplication application = new SpringApplication(MySpringBoot4ProjectApplication.class);
+        //WebApplication type 변경
+        application.setWebApplicationType(WebApplicationType.NONE);
+        application.run(args);
     }
 
 
---
+++ src/main/java/com/rookies6/myspringboot4project/runner/MyRunner.java
@@ -0,0 +1,34 @@
+package com.rookies6.myspringboot4project.runner;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Consumer;
+
+@Component
+public class MyRunner implements ApplicationRunner {
+    @Value("${spring.application.name}")
+    String applicationName;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("MyRunner run() 호출됨!!");
+        System.out.println("Application Name = " + applicationName);
+
+        // Consumer 인터페이스를 Anonymous Inner Class
+        args.getOptionNames().forEach(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println("arg name = " + s);
+            }
+        });
+
+        // args.getOptionNames() 메서드의 리턴타입 Set<String>
+        // Iterable의 forEach(Consumer)
+        // Consumer의 추상메서드 void accept(T t)
+        // Consumer 인터페이스를 람다식으로 표현
+        args.getOptionNames().forEach(name -> System.out.println("name = " + name));
+    }
+}
