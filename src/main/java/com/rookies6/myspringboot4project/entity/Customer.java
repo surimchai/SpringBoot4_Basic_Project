51758656c8778ab2eada7c6e835ec3e6d6c40fe1
surimchai <slchai0903@gmail.com>
2026-09-01 14:48:37 +0900

    customer entity

---
+++ src/main/java/com/rookies6/myspringboot4project/entity/Customer.java
@@ -0,0 +1,19 @@
+package com.rookies6.myspringboot4project.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "customers")
+@Getter @Setter
+public class Customer {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String customerId;
+
+    @Column(nullable = false)
+    private String customerName;
+}
