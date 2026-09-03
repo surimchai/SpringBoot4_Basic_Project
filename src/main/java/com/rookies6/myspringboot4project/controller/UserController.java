ab7d197848e26da2c5fd0114e9d5dbe78f025155
surimchai <slchai0903@gmail.com>
2026-09-03 14:46:32 +0900

    add-user html add

---
+++ src/main/java/com/rookies6/myspringboot4project/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.rookies6.myspringboot4project.controller;
+
+import com.rookies6.myspringboot4project.entity.User;
+import com.rookies6.myspringboot4project.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+    private final UserRepository userRepository;
+
+    @GetMapping("/thymeleaf")
+    public String leaf(Model model) {
+        model.addAttribute("name","스프링부트");
+        return "leaf";
+    }
+    /*
+        public ModelAndView(String viewName, String modelName, Object modelObject)
+     */
+    @GetMapping("/index")
+    public ModelAndView userList() {
+        List<User> userList = userRepository.findAll();
+        return new ModelAndView("index","users",userList);
+    }
+
+    @GetMapping("/signup")
+    public String showSignUpForm(){
+        return "add-user";
+    }
+
+}
--- src/main/java/com/rookies6/myspringboot4project/entity/User.java
+++ src/main/java/com/rookies6/myspringboot4project/entity/User.java
@@ -15,8 +15,10 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     private String name;
+
     @Column(unique = true, nullable = false)
     private String email;
 
