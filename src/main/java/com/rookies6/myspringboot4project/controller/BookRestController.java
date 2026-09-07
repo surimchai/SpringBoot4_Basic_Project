de20e1ed79579744bd0d16f4aa1863dedeedb939
surimchai <slchai0903@gmail.com>
2026-09-07 13:35:56 +0900

    예외처리 수정

---
+++ src/main/java/com/rookies6/myspringboot4project/controller/BookRestController.java
@@ -0,0 +1,12 @@
+//package com.rookies6.myspringboot4project.controller;
+//
+//import com.rookies6.myspringboot4project.repository.BookRepository;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/api/books")
+//public class BookRestController {
+//    //private final BookRepository bookRepository;
+//
+//}
